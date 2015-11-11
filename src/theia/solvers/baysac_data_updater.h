5d583d39780d2bf304f719399c67c7300f3e1bb6
Richard Klemm <richy@coding-reality.de>
2015-11-11 17:08:38 +0100

    Fix optimized baysac
    Change-Id: Ib65745e9a792a69535a57087bbaae39a830e9d0d

--- src/theia/solvers/baysac_data_updater.h
+++ src/theia/solvers/baysac_data_updater.h
@@ -2,6 +2,7 @@
 #define THEIA_SOLVERS_BAYSAC_DATA_UPDATER_H_
 
 #include <cmath>
+#include <iostream>
 #include <algorithm>
 #include <limits>
 #include <type_traits>
@@ -9,60 +10,59 @@
 #include "theia/solvers/data_updater.h"
 
 namespace theia {
-template <class Datum> class BaySacDataUpdater: public DataUpdater<Datum> {
-    public:
-        typedef typename std::vector<Datum> Data;
+template <class Datum>
+class BaySacDataUpdater : public DataUpdater<Datum> {
+   public:
+    typedef typename std::vector<Datum> Data;
 
-        virtual ~BaySacDataUpdater() {};
+    virtual ~BaySacDataUpdater(){};
 
-        virtual void UpdateData(Data &data,
-                std::vector<std::reference_wrapper<Datum> >& modelSampleset,
-                int num_iterations, double inlier_ratio,
-                int min_num_samples) override {
+    virtual void UpdateData(
+        Data& data, std::vector<std::reference_wrapper<Datum> >& modelSampleset,
+        int num_iterations, double inlier_ratio, int min_num_samples) override {
+        float sums = std::accumulate(
+            std::begin(data), std::end(data), 0.0f,
+            [](float a, const Datum& b) { return a + b.probability; });
 
-            float max = data[0].probability;
+        // Normalize probabilities
+        for (auto& match : data) {
+            match.probability /= sums;
+        }
 
-            for(auto& match: data) {
-                match.probability /= max;
-            }
+        sums = std::accumulate(
+            data.begin(), data.end(), 0.0f,
+            [](float a, const Datum& b) { return a + b.probability; });
 
-            double modelProb =
-              1.0 - pow(1.0 - pow(inlier_ratio, min_num_samples),
-                        num_iterations);
-
-            VLOG(3) << "\n\n\n\n";
-            VLOG(3) << "ModelProb: " << modelProb;
-            VLOG(3) << "Data[0] probability: " << data[0].probability;
-            VLOG(3) << "NUM Inliers: " << modelSampleset.size();
-
-            /*
-            inlier_ratio = std::max(inlier_ratio,
-                    inlier_ratio + std::numeric_limits<float>::epsilon());
+        /*
+        double modelProb =
+            1.0 - pow(1.0 - pow(inlier_ratio, min_num_samples), num_iterations);
             */
-            float probabilityMul = 1.0;
-            // float probabilityMul = 1.0f;
-            for(auto& inlier : modelSampleset) {
-                probabilityMul *= inlier.get().probability;
-            }
-            probabilityMul *= std::max(modelProb, modelProb + std::numeric_limits<float>::epsilon());
 
-            for(auto& inlier : modelSampleset) {
-                float newProb = (inlier.get().probability - probabilityMul) /
-                                      1.0f - probabilityMul;
-                inlier.get().probability = newProb;
-                VLOG(3) << "NewProb: " << inlier.get().probability;
-            };
+        // Likelihood ≈ k/D
+        // k = number inliers, D = Number of samples
+        // k/D = inlier_ratio
 
-            std::sort(data.begin(), data.end(), [] (Datum& lhs, Datum& rhs) -> bool {
-                return lhs.probability > rhs.probability;
-            });
+        inlier_ratio = std::max(
+            inlier_ratio, inlier_ratio + std::numeric_limits<float>::epsilon());
 
-            // Normalize
-
-            VLOG(3) << "Data[0] probability: " << data[0].probability;
-            VLOG(3) << "Data[0] x, y: " << data[0].feature1.x() << " : " << data[0].feature1.y();
+        for (auto& inlier : modelSampleset) {
+            // An optimized BaySac Algorithm
+            // P(i) = P(i)_{t-1} * k/D
+            float newProb = inlier.get().probability * inlier_ratio;
+            inlier.get().probability = newProb;
         };
+
+        std::sort(data.begin(), data.end(), [](Datum& lhs, Datum& rhs) -> bool {
+            return lhs.probability > rhs.probability;
+        });
+
+        // Normalize
+
+        VLOG(3) << "Data[0] probability: " << data[0].probability;
+        VLOG(3) << "Data[0] x, y: " << data[0].feature1.x() << " : "
+                << data[0].feature1.y();
     };
+};
 }  // namespace theia
 
 #endif  // THEIA_SOLVERS_BAYSAC_DATA_UPDATER_H_
